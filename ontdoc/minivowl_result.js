d1c32b3e9bc5ae251615f01486258854c83f9785
situx <situx@users.noreply.github.com>
2023-04-26 17:41:41 +0000

    Deploying to gh-pages from @ situx/samian-lod@89e1bdab397bc3581cd8281cb5c74f87c3d0e08a 🚀

---
+++ ontdoc/minivowl_result.js
@@ -0,0 +1,28 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "Place",
+   "type": "class",
+   "uri": "http://archaeology.link/ontology#Place"
+  },
+  {
+   "name": "Activity",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/prov#Activity"
+  }
+ ],
+ "links": [
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "wasGeneratedBy",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/prov-o/wasGeneratedBy"
+  }
+ ]
+}
